800ffc7be0716466a490a2f545896fa1fbf383cc
Hixo <hixohixo32@gmail.com>
2023-04-08 23:09:31 +0200

    init project

--- src/components/Errors/404.tsx
+++
@@ -1,7 +0,0 @@
-export const NoMatch = () => {
-    return (
-        <div className="w-screen h-screen bg-purple-500 flex justify-center items-center font-poppins text-white font-bold">
-            <h2 className="text-9xl">404</h2>
-        </div>
-    )
-}
