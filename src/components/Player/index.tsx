d2ad0055271c3b5ad90bba4c5d7cb32d24c69b4e
Hixo <hixohixo32@gmail.com>
2023-04-10 11:50:42 +0200

    clear tailwind classes

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -16,7 +16,7 @@ export const Player = () => {
           <div className="bg-[#edecff] p-4 m-4 h-[50%] rounded-lg">
             <div className="flex items-center gap-4 mt-2 justify-center">
               <img
-                className="w-16"
+                className="w-16 h-16"
                 src={`https://mc-heads.net/avatar/${playerInfo.uuid}`}
                 alt=""
               />
@@ -37,7 +37,7 @@ export const Player = () => {
             <div className={`bg-[#4b4168] h-auto mb-4 rounded-lg ${bedwarsActive ? 'pt-4' : "py-4"}`}>
               <div className="flex justify-between items-center">
               <p className="font-poppins text-white font-bold px-4 text-3xl">Bedwars</p>
-              <span onClick={() => setBedwarsActive(!bedwarsActive)} className={`font-bold text-4xl font-poppins text-white  mr-6 cursor-pointer select-none duration-300 transition-transform ${bedwarsActive ? '-rotate-90' : 'rotate-90'}`}>{'>'}</span> 
+              <span onClick={() => setBedwarsActive(!bedwarsActive)} className={`arrow ${bedwarsActive ? '-rotate-90' : 'rotate-90'}`}>{'>'}</span> 
               </div>          
               <motion.div
               animate={bedwarsActive ? "open" : "closed"}
@@ -45,25 +45,25 @@ export const Player = () => {
                 open: { opacity: 1, x: 0 },
                 closed: { opacity: 0 },
               }}
-              className={`w-full h-full bg-[#edecff] mt-4 flex-col font-poppins text-[#4b4168] my-2 p-4 rounded-b-lg ${bedwarsActive ? "flex" : "hidden"}`}>
-                <p className="px-6 my-1 font-bold">Zabójstwa: <span className="font-normal">{bedwarsStats.kills}</span></p>
-                <p className="px-6 my-1 font-bold">Śmierci: <span className="font-normal">{bedwarsStats.deaths}</span></p>
-                <p className="px-6 my-1 font-bold">Zdobyte materiały: <span className="font-normal">{bedwarsStats.collectedRes}</span></p>
-                <p className="px-6 my-1 font-bold">Monety: <span className="font-normal">{bedwarsStats.coins}</span></p>
-                <p className="px-6 my-1 font-bold">Doświadczenie: <span className="font-normal">{bedwarsStats.exp}</span></p>
-                <p className="px-6 my-1 font-bold">Zagrane gry: <span className="font-normal">{bedwarsStats.games_played}</span></p>
-                <p className="px-6 my-1 font-bold">Zniszczone łózka: <span className="font-normal">{bedwarsStats.brokenBeds}</span></p>
-                <p className="px-6 my-1 font-bold">Stracone łózka: <span className="font-normal">{bedwarsStats.lostBeds}</span></p>
-                <p className="px-6 my-1 font-bold">Wygrane: <span className="font-normal">{bedwarsStats.wins}</span></p>
-                <p className="px-6 my-1 font-bold">Kupione itemy: <span className="font-normal">{bedwarsStats.purshasedItems}</span></p>
-                <p className="px-6 my-1 font-bold">KD: <span className="font-normal">{bedwarsStats.kd.toFixed(2)}</span></p>
-                <p className="px-6 my-1 font-bold">Finałowe zabójstwa: <span className="font-normal">{bedwarsStats.finalKills}</span></p>
+              className={`stats ${bedwarsActive ? "flex" : "hidden"}`}>
+                <p className="stats-item">Zabójstwa: <span className="font-normal">{bedwarsStats.kills}</span></p>
+                <p className="stats-item">Śmierci: <span className="font-normal">{bedwarsStats.deaths}</span></p>
+                <p className="stats-item">Zdobyte materiały: <span className="font-normal">{bedwarsStats.collectedRes}</span></p>
+                <p className="stats-item">Monety: <span className="font-normal">{bedwarsStats.coins}</span></p>
+                <p className="stats-item">Doświadczenie: <span className="font-normal">{bedwarsStats.exp}</span></p>
+                <p className="stats-item">Zagrane gry: <span className="font-normal">{bedwarsStats.games_played}</span></p>
+                <p className="stats-item">Zniszczone łózka: <span className="font-normal">{bedwarsStats.brokenBeds}</span></p>
+                <p className="stats-item">Stracone łózka: <span className="font-normal">{bedwarsStats.lostBeds}</span></p>
+                <p className="stats-item">Wygrane: <span className="font-normal">{bedwarsStats.wins}</span></p>
+                <p className="stats-item">Kupione itemy: <span className="font-normal">{bedwarsStats.purshasedItems}</span></p>
+                <p className="stats-item">KD: <span className="font-normal">{bedwarsStats.kd.toFixed(2)}</span></p>
+                <p className="stats-item">Finałowe zabójstwa: <span className="font-normal">{bedwarsStats.finalKills}</span></p>
                 </motion.div>     
            </div>
             <div className={`bg-[#4b4168] h-auto mb-4 rounded-lg ${skywarsActive ? 'pt-4' : "py-4"}`}>
               <div className="flex justify-between items-center">
               <p className="font-poppins text-white font-bold px-4 text-3xl">Skywars</p>
-              <span onClick={() => setSkywarsActive(!skywarsActive)} className={`font-bold text-4xl font-poppins text-white  mr-6 cursor-pointer select-none duration-300 transition-transform ${skywarsActive ? '-rotate-90' : 'rotate-90'}`}>{'>'}</span> 
+              <span onClick={() => setSkywarsActive(!skywarsActive)} className={`arrow ${skywarsActive ? '-rotate-90' : 'rotate-90'}`}>{'>'}</span> 
               </div>          
               <motion.div
               animate={skywarsActive ? "open" : "closed"}
@@ -71,16 +71,16 @@ export const Player = () => {
                 open: { opacity: 1, x: 0 },
                 closed: { opacity: 0 },
               }}
-              className={`w-full h-full bg-[#edecff] mt-4 flex-col font-poppins text-[#4b4168] my-2 p-4 rounded-b-lg ${skywarsActive ? "flex" : "hidden"}`}>
-                <p className="px-6 my-1 font-bold">Zabójstwa: <span className="font-normal">{skywarsStats.kills}</span></p>
-                <p className="px-6 my-1 font-bold">Śmierci: <span className="font-normal">{skywarsStats.deaths}</span></p>
-                <p className="px-6 my-1 font-bold">Zagrane gry: <span className="font-normal">{skywarsStats.games_played}</span></p>
-                <p className="px-6 my-1 font-bold">Monety: <span className="font-normal">{skywarsStats.coins}</span></p>
-                <p className="px-6 my-1 font-bold">Ostatni tryb: <span className="font-normal">{skywarsStats.lastMode}</span></p>
-                <p className="px-6 my-1 font-bold">Wygrane: <span className="font-normal">{skywarsStats.wins}</span></p>
-                <p className="px-6 my-1 font-bold">Przegrane: <span className="font-normal">{skywarsStats.losses}</span></p>
-                <p className="px-6 my-1 font-bold">KD: <span className="font-normal">{(skywarsStats.kills / skywarsStats.deaths).toFixed(2)}</span></p>
-                <p className="px-6 my-1 font-bold">Dusze: <span className="font-normal">{skywarsStats.souls}</span></p>
+              className={`stats ${skywarsActive ? "flex" : "hidden"}`}>
+                <p className="stats-item">Zabójstwa: <span className="font-normal">{skywarsStats.kills}</span></p>
+                <p className="stats-item">Śmierci: <span className="font-normal">{skywarsStats.deaths}</span></p>
+                <p className="stats-item">Zagrane gry: <span className="font-normal">{skywarsStats.games_played}</span></p>
+                <p className="stats-item">Monety: <span className="font-normal">{skywarsStats.coins}</span></p>
+                <p className="stats-item">Ostatni tryb: <span className="font-normal">{skywarsStats.lastMode}</span></p>
+                <p className="stats-item">Wygrane: <span className="font-normal">{skywarsStats.wins}</span></p>
+                <p className="stats-item">Przegrane: <span className="font-normal">{skywarsStats.losses}</span></p>
+                <p className="stats-item">KD: <span className="font-normal">{(skywarsStats.kills / skywarsStats.deaths).toFixed(2)}</span></p>
+                <p className="stats-item">Dusze: <span className="font-normal">{skywarsStats.souls}</span></p>
                 </motion.div>     
            </div>
           </div>
